d8edfb82cce5dc84b89a681e68e8c8beb4510952
Tomek Bar <tomasz.bar@deviniti.com>
2020-01-21 14:17:12 +0100

    change tests

---
+++ src/test/java/com/deviniti/ParametrizedTest.kt
@@ -0,0 +1,22 @@
+package com.deviniti
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+
+class ParametrizedTest(val n: Int) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "test: {0}")
+        fun testParameters(): List<Int> {
+            return (1..5000).toList()
+        }
+    }
+
+    @Test
+    fun test(){
+        print("test $n")
+    }
+}
