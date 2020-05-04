90d0c60c51887390c7d62a8fe7df914a97e7929b
Tomek <tomasz.bar3@gmail.com>
2020-05-04 17:16:53 +0200

    ds

--- src/test/java/com/deviniti/ParametrizedTest.kt
+++ src/test/java/com/deviniti/ParametrizedTest.kt
@@ -1,22 +1,17 @@
-//package com.deviniti
-//
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//import org.junit.runners.Parameterized
-//
-//@RunWith(Parameterized::class)
-//
-//class ParametrizedTest(val n: Int) {
-//    companion object {
-//        @JvmStatic
-//        @Parameterized.Parameters(name = "test: {0}")
-//        fun testParameters(): List<Int> {
-//            return (1..2000).toList()
-//        }
-//    }
-//
-////    @Test
-////    fun test(){
-////        print("test $n")
-////    }
-//}
+package com.deviniti
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+
+class ParametrizedTest(val n: Int) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "test: {0}")
+        fun testParameters(): List<Int> {
+            return (1..2000).toList()
+        }
+    }
+}
